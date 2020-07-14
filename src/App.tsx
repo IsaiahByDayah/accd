fb793c56ef0d1b71006696d4ddba60a3284aa3ed
Isaiah Smith <jordankid93@gmail.com>
2020-07-13 22:23:35 -0700

    chore: outline header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./lib/bootstrap"
 
 import React, { FunctionComponent } from "react"
+import { AppBar, Toolbar, Typography, Box, TextField, InputAdornment } from "@material-ui/core"
+import { SearchRounded } from "@material-ui/icons"
 
 import Base from "./lib/Base"
 
@@ -8,6 +10,35 @@ import HelloWorld from "./components/HelloWorld"
 
 const App: FunctionComponent = () => (
     <Base>
+        <AppBar elevation={0}>
+            <Toolbar>
+                <Typography variant="h6" style={{ fontWeight: 900, width: "100%" }}>
+                    Animal Crossing Custom Designs
+                </Typography>
+
+                <Box>
+                    <Box
+                        style={{
+                            borderRadius: "1em/50%",
+                            background: "rgba(255,255,255,0.5)",
+                            padding: "0px 1em ",
+                        }}
+                    >
+                        <TextField
+                            InputProps={{
+                                disableUnderline: true,
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <SearchRounded />
+                                    </InputAdornment>
+                                ),
+                            }}
+                            placeholder="Search..."
+                        />
+                    </Box>
+                </Box>
+            </Toolbar>
+        </AppBar>
         <HelloWorld />
     </Base>
 )
