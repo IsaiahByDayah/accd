4d41dbda4774f4df94a351f2c2bd88cec74743eb
Isaiah Smith <jordankid93@gmail.com>
2020-07-17 23:11:57 -0700

    wip

--- src/components/Prompt.stories.tsx
+++ src/components/Prompt.stories.tsx
@@ -1,15 +1,27 @@
 import { storiesOf } from "@storybook/react"
 import React from "react"
-import { boolean, text } from "@storybook/addon-knobs"
+import { boolean, text, select } from "@storybook/addon-knobs"
 import { action } from "@storybook/addon-actions"
 
-import { PromptBase } from "components/Prompt"
+import { PromptBase, LoginPrompt, ConfirmationPrompt } from "components/Prompt"
 
-storiesOf("Components", module).add("Prompt", () => (
-    <PromptBase
-        open={boolean("Open?", false)}
-        title={text("Title", "Prompt Title")}
-        description={text("Description", "Description for this prompt is displayed here.")}
-        onClose={action("On Close")}
-    />
-))
+storiesOf("Components/Prompt", module)
+    .add("Base", () => (
+        <PromptBase
+            open={boolean("Open?", false)}
+            title={text("Title", "Prompt Title")}
+            description={text("Description", "Description for this prompt is displayed here.")}
+            onClose={action("On Close")}
+        />
+    ))
+    .add("Login", () => (
+        <LoginPrompt user={text("User", "John Doe")} reason={select("Reason", ["like", "comment"], "like")} />
+    ))
+    .add("Confirmation", () => (
+        <ConfirmationPrompt
+            title={text("Title", "Are you sure?")}
+            description={text("Description", "")}
+            onConfirm={action("On Confirm")}
+            onClose={boolean("Can Close?", false) ? action("On Close") : undefined}
+        />
+    ))
--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,17 +1,35 @@
 import React, { FC, ReactNode } from "react"
-import { makeStyles, Modal, Paper, Container, IconButton, Typography } from "@material-ui/core"
-import { CloseRounded } from "@material-ui/icons"
+import { makeStyles, Modal, Paper, Box, Container, IconButton, Typography } from "@material-ui/core"
+import { CloseRounded, FavoriteRounded, ChatBubbleRounded, LiveHelpRounded, BeenhereRounded } from "@material-ui/icons"
 
-const useStyles = makeStyles(({ spacing }) => ({
+import Button from "components/Button"
+
+const useStyles = makeStyles(({ spacing, breakpoints }) => ({
     root: {
         display: "flex",
         placeItems: "center",
     },
     paper: {
         position: "relative",
+
+        [breakpoints.down("xs")]: {
+            height: "100vh",
+            width: "100vw",
+        },
     },
-    title: {},
-    description: {},
+    container: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        placeItems: "center",
+    },
+    title: {
+        fontWeight: "bold",
+        marginBottom: spacing(2),
+    },
+    description: {
+        marginBottom: spacing(2),
+    },
     closeButton: {
         position: "absolute",
         top: spacing(),
@@ -33,12 +51,22 @@ export const PromptBase: FC<PromptProps> = ({ open, header, title, description, 
     return (
         <Modal className={classes.root} open={open}>
             <Paper className={classes.paper}>
-                <Container>
-                    {header}
-                    {title && <Typography className={classes.title}>{title}</Typography>}
-                    {description && <Typography className={classes.description}>{description}</Typography>}
-                    {children}
-                </Container>
+                <Box className={classes.container} px={2} py={6}>
+                    <Container>
+                        {header}
+                        {title && (
+                            <Typography className={classes.title} variant="h5" align="center">
+                                {title}
+                            </Typography>
+                        )}
+                        {description && (
+                            <Typography className={classes.description} align="center" variant="body2">
+                                {description}
+                            </Typography>
+                        )}
+                        {children}
+                    </Container>
+                </Box>
                 {onClose && (
                     <IconButton className={classes.closeButton} onClick={onClose}>
                         <CloseRounded className={classes.closeIcon} />
@@ -49,8 +77,110 @@ export const PromptBase: FC<PromptProps> = ({ open, header, title, description, 
     )
 }
 
+const useLoginStyles = makeStyles(({ spacing }) => ({
+    loginButton: {
+        marginBottom: spacing(2),
+    },
+}))
+
+interface LoginPromptProps {
+    reason: "like" | "comment"
+    user: string
+}
+
+export const LoginPrompt: FC<LoginPromptProps> = ({ reason, user }) => {
+    const classes = useLoginStyles()
+
+    return (
+        <PromptBase
+            open={true}
+            title={reason === "like" ? "Like a design to show you care!" : "Reply to join the conversation!"}
+            description={
+                reason === "like"
+                    ? `Join now to let ${user} know you like their design.`
+                    : `Join now to respond to ${user}'s design.`
+            }
+            header={
+                <Box py={3} display="flex" justifyContent="center" alignItems="center">
+                    {reason === "like" ? (
+                        <>
+                            <FavoriteRounded color="secondary" />
+                            <FavoriteRounded fontSize="large" color="secondary" />
+                            <FavoriteRounded color="secondary" />
+                        </>
+                    ) : (
+                        <>
+                            <ChatBubbleRounded color="primary" />
+                            <ChatBubbleRounded fontSize="large" color="primary" />
+                            <ChatBubbleRounded color="primary" />
+                        </>
+                    )}
+                </Box>
+            }
+            onClose={() => null}
+        >
+            <Box py={4} display="flex" flexDirection="column">
+                <Button className={classes.loginButton} variant="contained" fullWidth>
+                    Log in
+                </Button>
+                <Button variant="outlined" fullWidth>
+                    Sign up
+                </Button>
+            </Box>
+        </PromptBase>
+    )
+}
+
+const useConfirmationStyles = makeStyles(({ spacing }) => ({
+    cancelButton: {
+        marginTop: spacing(2),
+    },
+}))
+
+interface ConfirmationPromptProps {
+    title?: string
+    description?: string
+    onConfirm: () => void
+    onClose?: () => void
+}
+
+export const ConfirmationPrompt: FC<ConfirmationPromptProps> = ({
+    title = "Are you sure?",
+    description,
+    onConfirm,
+    onClose,
+}) => {
+    const classes = useConfirmationStyles()
+
+    return (
+        <PromptBase
+            open={true}
+            title={title}
+            description={description}
+            header={
+                <Box py={3} display="flex" justifyContent="center" alignItems="center">
+                    <BeenhereRounded color="secondary" />
+                    <BeenhereRounded fontSize="large" color="primary" />
+                    <BeenhereRounded htmlColor="#FF9F1C" />
+                </Box>
+            }
+            onClose={onClose}
+        >
+            <Box py={4} display="flex" flexDirection="column">
+                <Button variant="contained" fullWidth onClick={onConfirm}>
+                    Confirm
+                </Button>
+                {onClose && (
+                    <Button className={classes.cancelButton} variant="outlined" fullWidth onClick={onClose}>
+                        Cancel
+                    </Button>
+                )}
+            </Box>
+        </PromptBase>
+    )
+}
+
 const Prompt = () => {
-    const classes = useStyles()
     return <PromptBase open={false} />
 }
 
